34707de8e65ac9c20000987c7dc32e88277adb67
Dmytro Verner <dmytro.verner@gmail.com>
2018-10-13 00:33:03 +0300

    Added Mkdir basic implementation

---
+++ src/com/dmytro_verner/commands/Mkdir.scala
@@ -0,0 +1,65 @@
+package com.dmytro_verner.commands
+import com.dmytro_verner.files.{DirEntry, Directory}
+import com.dmytro_verner.filesystem.State
+
+class Mkdir(name: String) extends Command {
+
+  override def apply(state: State): State = {
+    val wd = state.wd
+    if (wd.hasEntry(name)) {
+      state.setMessage("Entry " + name + " already exists")
+    } else if (name.contains(Directory.SEPARATOR)) {
+      state.setMessage(name + " must not contain separators")
+    } else if (checkIllegal(name)) {
+      state.setMessage(name + ": illegal entry name")
+    } else {
+      doMkdir(state, name)
+    }
+  }
+
+  def checkIllegal(str: String): Boolean = {
+    name.contains(".")
+  }
+
+  def doMkdir(state:State, str: String): State = {
+
+    def updateStructure(currentDirectory: Directory, path: List[String], newEntry: DirEntry): Directory = {
+      if (path.isEmpty) currentDirectory.addEntry(newEntry)
+      else {
+        val oldEntry = currentDirectory.findEntry(path.head).asDirectory
+        currentDirectory.replaceEntry(oldEntry.name, updateStructure(oldEntry, path.tail, newEntry))l
+      }
+
+      /*
+      /a/b
+        (contents)
+        (new entry) /d
+
+        newRoot = updateStructure(root, ["a", "b"], /d) = root.replaceEntry("a", updateStructure(/a, ["b"], /d) = /a.replaceEntry("b", updateStructure(/b, [], /d) = /b.add(/d)
+         => path.isEmpty?
+         => oldEntry = /a
+         root.replaceEntry("a", updateStructure(/a, ["b"], /d) = /a.replaceEntry("b", updateStructure(/b, [], /d) = /b.add(/d)
+          => path.isEmpty?
+          => oldEntry = /b
+            /a.replaceEntry("b", updateStructure(/b, [], /d) = /b.add(/d)
+              => path.isEmpty? => /b.add(/d)
+       */
+    }
+
+    val wd = state.wd
+
+    // all the directories in the full path
+    val allDirsInPath = wd.getAllFoldersInPath
+
+    // create new directory entry in the wd
+    val newDir = Directory.empty(wd.path, name)
+
+    // update the whole directory structure starting from the root
+    val newRoot = updateStructure(state.root, allDirsInPath, newDir)
+
+    // find new working directory instance given wd's full path, in the new directory structure
+    val newWd = newRoot.findDescendant(allDirsInPath)
+
+    State(newRoot, newWd)
+  }
+}
