aabf22c81483014b89f6341e593dd595d04a78e7
Dmytro Verner <dmytro.verner@gmail.com>
2018-10-13 15:26:30 +0300

    Completed Mkdir implementation and added Ls command

---
+++ src/com/dmytro_verner/commands/Ls.scala
@@ -0,0 +1,21 @@
+package com.dmytro_verner.commands
+
+import com.dmytro_verner.files.DirEntry
+import com.dmytro_verner.filesystem.State
+
+class Ls extends Command {
+
+  override def apply(state: State): State = {
+    val contents = state.wd.contents
+    val niceOutput = createNiceOutput(contents)
+    state.setMessage(niceOutput)
+  }
+
+  def createNiceOutput(contents: List[DirEntry]): String = {
+    if (contents.isEmpty) ""
+    else {
+      val entry = contents.head
+      entry.name + "[" + entry.getType + "]\n" + createNiceOutput(contents.tail)
+    }
+  }
+}
