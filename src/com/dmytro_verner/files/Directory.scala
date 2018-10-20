1b1281ef352d5de5deaeff54f4588c9ca0895372
Dmytro Verner <dmytro.verner@gmail.com>
2018-10-20 19:45:55 +0300

    Implemented basic Cd command functionality

--- src/com/dmytro_verner/files/Directory.scala
+++ src/com/dmytro_verner/files/Directory.scala
@@ -35,9 +35,15 @@ class Directory(override val parentPath: String, override val name: String, val 
   def replaceEntry(entryName: String, newEntry: DirEntry): Directory =
     new Directory(parentPath, name, contents.filter(e => !e.name.equals(entryName)) :+ newEntry)
 
+  def isRoot: Boolean = parentPath.isEmpty
+
   override def asDirectory: Directory = this
 
   override def asFile: File = throw new FileSystemException("Directory can't be converted to a file")
+
+  override def isDirectory: Boolean = true
+
+  override def isFile: Boolean = false
 
   def getType: String = "Directory"
 }
