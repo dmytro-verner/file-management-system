f187d321e86ea6251bdd85b3468b65f10a44c9bd
Dmytro Verner <dmytro.verner@gmail.com>
2018-11-03 23:48:38 +0200

    Refactored FileSystem command prompt to functional style

--- src/com/dmytro_verner/filesystem/FileSystem.scala
+++ src/com/dmytro_verner/filesystem/FileSystem.scala
@@ -1,19 +1,16 @@
 package com.dmytro_verner.filesystem
 
-import java.util.Scanner
-
 import com.dmytro_verner.commands.Command
 import com.dmytro_verner.files.Directory
 
 object FileSystem extends App {
 
   val root = Directory.ROOT
-  var state = State(root, root)
-  val scanner = new Scanner(System.in)
 
-  while (true) {
+  State(root, root).show
+  io.Source.stdin.getLines().foldLeft(State(root, root))((currentState, newLine) => {
+    val state = Command.from(newLine).apply(currentState)
     state.show
-    val input = scanner.nextLine()
-    state = Command.from(input).apply(state)
-  }
+    state
+  })
 }
